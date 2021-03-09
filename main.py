139abf2a6a7944bae31ded745143ea48b03c29f9
Jake Lira <llira09@gmail.com>
2021-03-09 15:43:51 -0600

    added game logic that shows possible moves

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE
-from checkers.board import Board
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
+
+from checkers.game import Game
 
 FPS = 60
 
@@ -18,10 +19,13 @@ def get_row_col_from_mouse(pos):
 def main():
     run = True
     clock = pygame.time.Clock()
-    board = Board()
+    game = Game(WIN)
 
     while run:
         clock.tick(FPS)
+        if game.winner() != None:
+            print(game.winner())
+            run = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -30,11 +34,8 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
-                piece = board.get_piece(row, col)
-                board.move(piece, 4, 3)
-
-        board.draw(WIN)
-        pygame.display.update()
+                game.select(row, col)
+        game.update()
 
     pygame.quit()
 
